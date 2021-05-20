053d4e48a290ffca99f2867f38266f2b45a357f4
Micah Levine <micah.levine@outlook.com>
2021-05-20 10:04:35 -0400

    created header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = (props) => (
+  <header className="App-header">
+    <ul className="container">
+      <li>Music Lists</li>
+    </ul>
+  </header>
+);
+
+export default Header;
